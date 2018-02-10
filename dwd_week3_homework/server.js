84abc320693c1116a1a1b08ea064e5798fd6a7a8
mengzhenxiao <mengzhenxiao@users.noreply.github.com>
2018-02-10 14:46:19 -0500

    week3 first success

---
+++ dwd_week3_homework/server.js
@@ -0,0 +1,27 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+	res.sendFile("moodpalette.html", {root: './public'}); // Files inside "public" folder
+});
+
+
+// index page
+
+app.get('/templatetest', function(req, res) {
+	var data = {person: {name: req.query.inputEmail, other: req.query.inputPassword}};
+    res.render('pages/index', data);
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(3000);
+console.log('3000 is the magic port');
